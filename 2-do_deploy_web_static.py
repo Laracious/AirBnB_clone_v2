60b30ab46f27f48a6d1561ee6da1b184c10524e1
Laracious <omolaramoke22@gmail.com>
2023-05-03 15:21:19 -0700

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,44 +1,49 @@
 #!/usr/bin/python3
-"""distributes an archive to your web servers"""
-
-from fabric.api import *
-from os import path
+""" This script distributes an archive to webservers"""
+from fabric.api import local, put, run, env
 from datetime import datetime
-from shlex import split
+from os.path import isfile
 
-env.user = 'ubuntu'
-env.hosts = ['34.74.70.205', '34.224.30.177']
+env.hosts = ["100.25.30.148", "35.175.105.87"]
+env.user = "ubuntu"
 
+
+def do_pack():
+    """ Generates a .tgz archive """
+
+    time_now = datetime.now().strftime("%Y%m%d%H%M%S")
+    filename = "web_static_{}.tgz".format(time_now)
+    archive_path = "versions/{}".format(filename)
+
+    print("Packing web_static to {}".format(archive_path))
+
+    local("mkdir -p versions")
+    local("tar -cvzf {} web_static".format(archive_path))
+
+    print("Successfully packed web_static to {}".format(archive_path))
+
+    return archive_path
 
 def do_deploy(archive_path):
-    """Deplay the archive tgz"""
-    if not path.exists(archive_path) or (
-                                         path.exists(archive_path) and
-                                         path.isdir(archive_path)):
+    """ Distributes an archive to webserver """
+
+    if not isfile(archive_path):
         return False
-    try:
-        put(archive_path, '/tmp/')
-        name_file_ext = archive_path.split("/")[1]
-        name_file = name_file_ext.split(".")[0]
-        cmd_mkdir = 'mkdir -p /data/web_static/releases/{}'.format(name_file)
-        run(cmd_mkdir)
-        cmd_uncom = 'tar -xzf /tmp/{}'.format(name_file_ext)
-        cmd_uncom += ' -C /data/web_static/releases/{}'.format(name_file)
-        run(cmd_uncom)
-        cmd_rm_file = 'rm /tmp/{}'.format(name_file_ext)
-        run(cmd_rm_file)
-        cmd_mv = 'mv /data/web_static/releases/'
-        cmd_mv += '{}/web_static/*'.format(name_file)
-        cmd_mv += ' /data/web_static/releases/{}/'.format(name_file)
-        run(cmd_mv)
-        cmd_rm_dir = 'rm -rf /data/web_static/releases/{}'.format(name_file)
-        cmd_rm_dir += '/web_static'
-        run(cmd_rm_dir)
-        run('rm -rf /data/web_static/current')
-        cmd_cre_sym = 'ln -s /data/web_static/releases/{}/'.format(name_file)
-        cmd_cre_sym += ' /data/web_static/current'
-        run(cmd_cre_sym)
-        print("New version deployed!")
-        return True
-    except:
-        return False
+
+    print("Deploying new version")
+
+    archive_name = archive_path.split("/")[-1]
+    folder_name = archive_name[: -4]
+    dir_path = "/data/web_static/releases/{}".format(folder_name)
+
+    put(archive_path, "/tmp/")
+    run("mkdir -p {}".format(dir_path))
+    run("tar -xzf /tmp/{} -C {}".format(archive_name, dir_path))
+    run("cp -r {}/web_static/* {}".format(dir_path, dir_path))
+    run("rm -rf /tmp/{} {}/web_static".format(archive_name, dir_path))
+    run("rm -rf /data/web_static/current")
+    run("ln -s {} /data/web_static/current".format(dir_path))
+
+    print("New version deployed!")
+
+    return True
